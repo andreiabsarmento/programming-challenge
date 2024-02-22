4dcd217bead76673fea3a73f748d9e537c693a52
andreiabsarmento <andreia.bsarmento@gmail.com>
2024-02-22 16:11:19 +0000

    -Added the functions call for weather and countries csv files

--- src/main/java/de/bcxp/challenge/App.java
+++ src/main/java/de/bcxp/challenge/App.java
@@ -1,5 +1,10 @@
 package de.bcxp.challenge;
 
+import de.bcxp.challenge.service.CountriesService;
+import de.bcxp.challenge.service.WeatherService;
+
+import java.io.IOException;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -8,16 +13,24 @@ public final class App {
 
     /**
      * This is the main entry method of your program.
+     *
      * @param args The CLI arguments passed
      */
     public static void main(String... args) {
 
-        // Your preparation code …
+        try {
+            WeatherService weatherService = new WeatherService();
+            System.out.printf("Day with smallest temperature spread: %s%n", weatherService.read());
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread: %s%n", dayWithSmallestTempSpread);
+        } catch (IOException ioException) {
+            System.err.println(ioException.getMessage());
+        }
+        try {
+            CountriesService countriesService = new CountriesService();
+            System.out.printf("Country with highest population density: %s%n", countriesService.read());
 
-        String countryWithHighestPopulationDensity = "Some country"; // Your population density analysis function call …
-        System.out.printf("Country with highest population density: %s%n", countryWithHighestPopulationDensity);
+        } catch (IOException ioException) {
+            System.err.println(ioException.getMessage());
+        }
     }
 }
